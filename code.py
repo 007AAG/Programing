ce62984c9c2c973bfa41f2a33a01f24c35b2543a
007AAG <aagneydath@gmail.com>
2026-04-01 20:40:10 +1300

    Half way with global

--- code.py
+++ code.py
@@ -1,17 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
+
+hire_details = []
+total_entries = 0
 
 def quit_app():
     main_window.destroy()
 
-def entry_print():
+def append_name():
     global hire_details, total_entries
 
-    hire_details.append([
-        entry_full_name.get(),
-        entry_receipt_number.get(),
-        entry_items_hired.get(),
-        entry_number_hired.get(),
-        entry_date_hired.get(),
-        entry_date_return.get(),
-    ])
-    total_entries += 1
+    name = entry_full_name.get()
+    receipt = entry_receipt_number.get()
+    item = entry_items_hired.get()
+    quantity = entry_number_hired.get()
+    date_hired = entry_date_hired.get()
+    return_date = entry_date_return.get() 
+
+def clear_fields():
+    entry_full_name.delete(0, END)
+    entry_receipt_number.delete(0, END)
+    entry_items_hired.delete(0, END)
+    entry_number_hired.delete(0, END)
+    entry_date_hired.delete(0, END)
+    entry_date_return.delete(0, END)
