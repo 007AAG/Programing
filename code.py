4ebcfae6c328040afc674a1ff0e7a4db313af420
007AAG <aagneydath@gmail.com>
2026-04-01 20:11:22 +1300

    Print hello

--- code.py
+++ code.py
@@ -1,8 +1,1 @@
-from tkinter import *
-
-def quit():
-    main_window.destroy()
-
-def entry_print():
-    Label(main_window,text= entry_first_name.get()).grid(column=0,row=3,sticky=E)
-    Label(main)
+print('Hello')
