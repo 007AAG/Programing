ef69349da6a7ce5eec8e4af2d8df1e8abcefb117
007AAG <aagneydath@gmail.com>
2026-04-01 20:20:38 +1300

    Finished the first part

--- code.py
+++ code.py
@@ -4,24 +4,28 @@ import random
 def quit():
     main_window.destroy
 
-def entry_print():
-    Label(main_window,text= entry_full_name.get()).grid(column=0,row=6,sticky=E)
-    Label(main_window,text=entry_Receipt_number.get()).grid(column=1,row=6)
-    Label(main_window,text=entry_item_hired.get()).grid(column=2,row=6)
-    Label(main_window,text=entry_number_hired.get()).grid(column=3,row=6)
-
-
 def main():
-    Button(main_window, text="Quit",command= quit) .grid(column=0,row=1)
-    Button(main_window, text="Print",command=entry_print).grid(column=1,row=1)
+    Button(main_window, text="Quit",command= quit) .grid(column=7,row=5)
+    Button(main_window, text="Print",command=entry_print).grid(column=7,row=2)
     Label(main_window,text="Customer Name").grid(column=0,row=2)
     Label(main_window,text="Receipt Number").grid(column=0,row=3)
     Label(main_window,text="Item Hired").grid(column=0,row=4)
     Label(main_window,text="Number Hired").grid(column=0,row=5)
     main_window.mainloop()
 
+def entry_print():
+    Label(main_window,text= entry_full_name.get()).grid(column=0,row=6,sticky=E)
+    Label(main_window,text=entry_receipt_number.get()).grid(column=1,row=6)
+    Label(main_window,text=entry_item_hired.get()).grid(column=2,row=6)
+    Label(main_window,text=entry_number_hired.get()).grid(column=3,row=6)
+
 main_window =Tk()
 entry_full_name= Entry(main_window)
 entry_full_name.grid(column=2,row=2,padx=10,pady=5)
-
+entry_receipt_number = Entry(main_window)
+entry_receipt_number.grid(column=2,row=3,padx=10,pady=5)
+entry_item_hired = Entry(main_window)
+entry_item_hired.grid(column=2,row=4,padx=10,pady=5)
+entry_number_hired = Entry(main_window)
+entry_number_hired.grid(column=2,row=5,padx=10,pady=5)
 main()
